7e7a918dfa41370212b9b79f8d23403458b8dc14
Ramon Tayag <ramon.tayag@gmail.com>
2016-06-28 11:00:31 +0800

    Re-record with new auth
    Filter out base64 auth properly

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,7 +4,13 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+
   %i[account_id api_key].each do |var|
     config.filter_sensitive_data("<#{var}>") { CONFIG[var]}
+  end
+
+  # VCR records the base 64 encoded version of the account id and api key
+  config.filter_sensitive_data("<auth_header>") do
+    Base64.encode64(CONFIG.values_at(:account_id, :api_key).join(":")).chomp
   end
 end
